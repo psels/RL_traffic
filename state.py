93209c01325146c14d88fcfedad7a7e0a4108670
arsene claustre <arsene.claustre@gmail.com>
2025-03-13 11:22:53 +0100

    Deep Q learning state : voiture en attente + voiture par voie

--- state.py
+++ state.py
@@ -1,11 +1,11 @@
 import traci
 
-def get_state(detector_ids):
-    state = []
-    for detector_id in detector_ids:
-        detector_state = traci.inductionloop.getLastStepOccupancy(detector_id) # on pourrait ajouter d'autres éléments
-        state.append(detector_state)
-    return state
+# def get_state(detector_ids):
+#     state = []
+#     for detector_id in detector_ids:
+#         detector_state = traci.inductionloop.getLastStepOccupancy(detector_id) # on pourrait ajouter d'autres éléments
+#         state.append(detector_state)
+#     return state
 
 def queue(lane_ids):
     state = []
@@ -14,4 +14,6 @@ def queue(lane_ids):
         state.append(lane_state)
     return state
 
-
+def get_state(lane_ids):
+    return [traci.lane.getLastStepHaltingNumber(lane_id) for i,lane_id in enumerate(lane_ids) if i>=12]+\
+        [traci.lane.getLastStepVehicleNumber(lane_id) for i,lane_id in enumerate(lane_ids) if i>=12]
